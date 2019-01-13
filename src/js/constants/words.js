3b69450b9ab6f5e73146af29b7ab8a9031524b4b
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2019-01-13 15:42:41 +0100

    words updated - 49 words

--- src/js/constants/words.js
+++ src/js/constants/words.js
@@ -3,31 +3,346 @@ import * as cat from "../constants/categories";
 const words = [
     {
         id: "1",
+        text: "auto",
+        category: cat.SINGLE,
+        image: "./images/auto.jpg",
+        sound: "./sounds/words/auto.mp3"
+    },
+    {
+        id: "2",
+        text: "bajka",
+        category: cat.SINGLE,
+        image: "./images/bajka.jpg",
+        sound: "./sounds/words/bajka.mp3"
+    },
+    {
+        id: "3",
+        text: "balon",
+        category: cat.SINGLE,
+        image: "./images/balon.jpg",
+        sound: "./sounds/words/balon.mp3"
+    },
+    {
+        id: "4",
+        text: "banan",
+        category: cat.SINGLE,
+        image: "./images/banan.jpg",
+        sound: "./sounds/words/banan.mp3"
+    },
+    {
+        id: "5",
+        text: "bobas",
+        category: cat.SINGLE,
+        image: "./images/bobas.jpg",
+        sound: "./sounds/words/bobas.mp3"
+    },
+    {
+        id: "6",
+        text: "buty",
+        category: cat.SINGLE,
+        image: "./images/buty.jpg",
+        sound: "./sounds/words/buty.mp3"
+    },
+    {
+        id: "7",
+        text: "byk",
+        category: cat.SINGLE,
+        image: "./images/byk.jpg",
+        sound: "./sounds/words/byk.mp3"
+    },
+    {
+        id: "8",
+        text: "flaga",
+        category: cat.SINGLE,
+        image: "./images/flaga.jpg",
+        sound: "./sounds/words/flaga.mp3"
+    },
+    {
+        id: "9",
+        text: "foka",
+        category: cat.SINGLE,
+        image: "./images/foka.jpg",
+        sound: "./sounds/words/foka.mp3"
+    },
+    {
+        id: "10",
+        text: "fotel",
+        category: cat.SINGLE,
+        image: "./images/fotel.jpg",
+        sound: "./sounds/words/fotel.mp3"
+    },
+    {
+        id: "11",
+        text: "hak",
+        category: cat.SINGLE,
+        image: "./images/hak.jpg",
+        sound: "./sounds/words/hak.mp3"
+    },
+    {
+        id: "12",
         text: "jajko",
         category: cat.SINGLE,
         image: "./images/jajko.jpg",
         sound: "./sounds/words/jajko.mp3"
     },
     {
-        id: "2",
+        id: "13",
+        text: "kawa",
+        category: cat.SINGLE,
+        image: "./images/kawa.jpg",
+        sound: "./sounds/words/kawa.mp3"
+    },
+    {
+        id: "14",
+        text: "kot",
+        category: cat.SINGLE,
+        image: "./images/kot.jpg",
+        sound: "./sounds/words/kot.mp3"
+    },
+    {
+        id: "15",
+        text: "koza",
+        category: cat.SINGLE,
+        image: "./images/koza.jpg",
+        sound: "./sounds/words/koza.mp3"
+    },
+    {
+        id: "16",
+        text: "krowa",
+        category: cat.SINGLE,
+        image: "./images/krowa.jpg",
+        sound: "./sounds/words/krowa.mp3"
+    },
+    {
+        id: "17",
+        text: "kura",
+        category: cat.SINGLE,
+        image: "./images/kura.jpg",
+        sound: "./sounds/words/kura.mp3"
+    },
+    {
+        id: "18",
+        text: "kwiat",
+        category: cat.SINGLE,
+        image: "./images/kwiat.jpg",
+        sound: "./sounds/words/kwiat.mp3"
+    },
+    {
+        id: "19",
         text: "lampa",
         category: cat.SINGLE,
         image: "./images/lampa.jpg",
         sound: "./sounds/words/lampa.mp3"
     },
     {
-        id: "3",
+        id: "20",
+        text: "las",
+        category: cat.SINGLE,
+        image: "./images/las.jpg",
+        sound: "./sounds/words/las.mp3"
+    },
+    {
+        id: "21",
+        text: "lis",
+        category: cat.SINGLE,
+        image: "./images/lis.jpg",
+        sound: "./sounds/words/lis.mp3"
+    },
+    {
+        id: "22",
+        text: "lody",
+        category: cat.SINGLE,
+        image: "./images/lody.jpg",
+        sound: "./sounds/words/lody.mp3"
+    },
+    {
+        id: "23",
+        text: "mama",
+        category: cat.SINGLE,
+        image: "./images/mama.jpg",
+        sound: "./sounds/words/mama.mp3"
+    },
+    {
+        id: "24",
+        text: "mapa",
+        category: cat.SINGLE,
+        image: "./images/mapa.jpg",
+        sound: "./sounds/words/mapa.mp3"
+    },
+    {
+        id: "25",
+        text: "motor",
+        category: cat.SINGLE,
+        image: "./images/motor.jpg",
+        sound: "./sounds/words/motor.mp3"
+    },
+    {
+        id: "26",
+        text: "motyl",
+        category: cat.SINGLE,
+        image: "./images/motyl.jpg",
+        sound: "./sounds/words/motyl.mp3"
+    },
+    {
+        id: "27",
+        text: "noc",
+        category: cat.SINGLE,
+        image: "./images/noc.jpg",
+        sound: "./sounds/words/noc.mp3"
+    },
+    {
+        id: "28",
         text: "nos",
         category: cat.SINGLE,
         image: "./images/nos.jpg",
         sound: "./sounds/words/nos.mp3"
     },
     {
-        id: "4",
+        id: "29",
+        text: "obraz",
+        category: cat.SINGLE,
+        image: "./images/obraz.jpg",
+        sound: "./sounds/words/obraz.mp3"
+    },
+    {
+        id: "30",
+        text: "oko",
+        category: cat.SINGLE,
+        image: "./images/oko.jpg",
+        sound: "./sounds/words/oko.mp3"
+    },
+    {
+        id: "31",
+        text: "osa",
+        category: cat.SINGLE,
+        image: "./images/osa.jpg",
+        sound: "./sounds/words/osa.mp3"
+    },
+    {
+        id: "32",
+        text: "owca",
+        category: cat.SINGLE,
+        image: "./images/owca.jpg",
+        sound: "./sounds/words/owca.mp3"
+    },
+    {
+        id: "33",
+        text: "palma",
+        category: cat.SINGLE,
+        image: "./images/palma.jpg",
+        sound: "./sounds/words/palma.mp3"
+    },
+    {
+        id: "34",
+        text: "panda",
+        category: cat.SINGLE,
+        image: "./images/panda.jpg",
+        sound: "./sounds/words/panda.mp3"
+    },
+    {
+        id: "35",
+        text: "pies",
+        category: cat.SINGLE,
+        image: "./images/pies.jpg",
+        sound: "./sounds/words/pies.mp3"
+    },
+    {
+        id: "36",
+        text: "ptak",
+        category: cat.SINGLE,
+        image: "./images/ptak.jpg",
+        sound: "./sounds/words/ptak.mp3"
+    },
+    {
+        id: "37",
+        text: "rekin",
+        category: cat.SINGLE,
+        image: "./images/rekin.jpg",
+        sound: "./sounds/words/rekin.mp3"
+    },
+    {
+        id: "38",
+        text: "robot",
+        category: cat.SINGLE,
+        image: "./images/robot.jpg",
+        sound: "./sounds/words/robot.mp3"
+    },
+    {
+        id: "39",
+        text: "rower",
+        category: cat.SINGLE,
+        image: "./images/rower.jpg",
+        sound: "./sounds/words/rower.mp3"
+    },
+    {
+        id: "40",
+        text: "ser",
+        category: cat.SINGLE,
+        image: "./images/ser.jpg",
+        sound: "./sounds/words/ser.mp3"
+    },
+    {
+        id: "41",
+        text: "smok",
+        category: cat.SINGLE,
+        image: "./images/smok.jpg",
+        sound: "./sounds/words/smok.mp3"
+    },
+    {
+        id: "42",
         text: "sowa",
         category: cat.SINGLE,
         image: "./images/sowa.jpg",
         sound: "./sounds/words/sowa.mp3"
+    },
+    {
+        id: "43",
+        text: "tata",
+        category: cat.SINGLE,
+        image: "./images/tata.jpg",
+        sound: "./sounds/words/tata.mp3"
+    },
+    {
+        id: "44",
+        text: "ufo",
+        category: cat.SINGLE,
+        image: "./images/ufo.jpg",
+        sound: "./sounds/words/ufo.mp3"
+    },
+    {
+        id: "45",
+        text: "usta",
+        category: cat.SINGLE,
+        image: "./images/usta.jpg",
+        sound: "./sounds/words/usta.mp3"
+    },
+    {
+        id: "46",
+        text: "wilk",
+        category: cat.SINGLE,
+        image: "./images/wilk.jpg",
+        sound: "./sounds/words/wilk.mp3"
+    },
+    {
+        id: "47",
+        text: "woda",
+        category: cat.SINGLE,
+        image: "./images/woda.jpg",
+        sound: "./sounds/words/woda.mp3"
+    },
+    {
+        id: "48",
+        text: "zebra",
+        category: cat.SINGLE,
+        image: "./images/zebra.jpg",
+        sound: "./sounds/words/zebra.mp3"
+    },
+    {
+        id: "49",
+        text: "zegar",
+        category: cat.SINGLE,
+        image: "./images/zegar.jpg",
+        sound: "./sounds/words/zegar.mp3"
     }
 ];
 
